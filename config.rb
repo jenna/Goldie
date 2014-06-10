1bd0376f4ce59b5c8c7d0b9039aa7dd061af51a9
jenna <jenhalpin@Jennifers-MacBook-Pro.local>
2014-06-10 00:13:18 -0700

    made new config.rb point to rwd theme folders

---
+++ config.rb
@@ -0,0 +1,15 @@
+require 'compass/import-once/activate'
+http_path = "/"
+css_dir = "/skin/frontend/rwd/mytheme/css"
+sass_dir = "/skin/frontend/rwd/mytheme/scss"
+images_dir = "/skin/frontend/rwd/mytheme/images"
+javascripts_dir = "/skin/frontend/rwd/mytheme/js"
+fonts_dir = "/skin/frontend/rwd/mytheme/fonts"
+ 
+add_import_path "./skin/frontend/rwd/default/scss"
+ 
+output_style = :nested
+color_output = false
+environment = :development
+
+preferred_syntax = :scss
